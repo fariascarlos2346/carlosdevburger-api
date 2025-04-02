289eaa625e30f20d0f684ba8e20f98766acfe90a
Carlos <fariascarlos2346@gmail.com>
2025-04-02 10:13:12 -0300

    não estou conseguindo abrir a imagem no navegador

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import routes from './routes';
 
+import { resolve } from 'node:path';
+
 import './database';
 
 
@@ -13,11 +15,15 @@ class App {
     }
 
     middlewares() {
-        this.app.use(express.json())
+        this.app.use(express.json());
+        this.app.use(
+            '/product-file', 
+            express.static(resolve(__dirname, '..', 'uploads')),
+        );
     }
 
     routes() {
-        this.app.use(routes)
+        this.app.use(routes);
     }
 }
 
--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -1,4 +1,5 @@
 import * as Yup from 'yup';
+import Product from '../models/Product';
 
 class ProductController {
     async store(request, response) {
@@ -7,14 +8,30 @@ class ProductController {
             price: Yup.number().required(),
             category: Yup.string().required(),
         });
-
+     
         try {
             schema.validateSync(request.body, { abortEarly: false });
         } catch(err) {
             return response.status(400).json({ error: err.errors })
         }
 
-        return response.status(201).json({ message: 'right' });
+        const { filename: path } = request.file;
+        const { name, price, category } = request.body;
+
+        const product = await Product.create({
+            name,
+            price,
+            category,
+            path,
+        });
+
+        return response.status(201).json(product);
+    }
+
+    async index(request, response) {
+        const products = await Product.findAll();
+
+        return response.json(products);
     }
 }
 
