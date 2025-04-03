03512e7d61ce9033fffeb507726db485acd68a50
Carlos <fariascarlos2346@gmail.com>
2025-04-02 23:32:45 -0300

    utilizando Jsonwebtoken para validar nossas rotas

--- src/config/multer.js
+++ src/config/multer.js
@@ -5,7 +5,7 @@ import { extname, resolve } from 'node:path';
 
 export default {
     storage: multer.diskStorage({
-        destination: resolve(__dirname, '..','..', 'upload'),
+        destination: resolve(__dirname, '..','..', 'uploads'),
         filename: (request, file, callback) => 
          callback(null, v4() + extname(file.originalname)),      
     }),
